f689cc53c7c6679dbdc004008358a395763bc0fa
paulo <paulohincar@gmail.com>
2022-06-27 21:16:15 -0300

    Add initial files

---
+++ src/main/java/com/paulo/projetospring/service/ClienteService.java
@@ -0,0 +1,16 @@
+package com.paulo.projetospring.service;
+
+import com.paulo.projetospring.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+}
